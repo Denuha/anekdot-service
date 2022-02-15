aeb171ff61622f618b9de409cd716e035f43b675
denis <polonskiyDD@kamaz.ru>
2022-02-15 14:28:58 +0300

    json description

--- internal/models/auth.go
+++ internal/models/auth.go
@@ -4,7 +4,7 @@ import "github.com/dgrijalva/jwt-go"
 
 type Claims struct {
 	jwt.StandardClaims
-	UserName string
-	Realm    string
-	ID       int
+	UserName string `json:"username"`
+	Realm    string `json:"realm"`
+	ID       int    `json:"id"`
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -3,21 +3,21 @@ package models
 import "time"
 
 type User struct {
-	ID         int64
-	UserName   string
-	ExternalID string
-	Realm      string
-	CreateTime time.Time
-	IsAdmin    bool `json:"-"`
+	ID         int64     `json:"id"`
+	UserName   string    `json:"username"`
+	ExternalID string    `json:"external_id"`
+	Realm      string    `json:"realm"`
+	CreateTime time.Time `json:"create_time"`
+	IsAdmin    bool      `json:"-"`
 }
 
 type UserRegistation struct {
 	UserLogin
-	Realm string
+	Realm string `json:"realm"`
 }
 
 type UserLogin struct {
-	Username string
-	Realm    string
-	Password string
+	Username string `json:"username"`
+	Realm    string `json:"realm"`
+	Password string `json:"password"`
 }
